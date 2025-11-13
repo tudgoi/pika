bcfc180e83fb80f3d051772779a423fae0c0bb8c
Arunkumar Dhananjayan <arunkd13@gmail.com>
2025-11-13 07:05:47 +0530

    Added CLI support

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+mod init;
+
+use std::path::PathBuf;
+
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(version)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Init { schema: PathBuf },
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::parse();
+
+    match args.command {
+        Commands::Init { schema } => init::run(schema),
+    }
 }

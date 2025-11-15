95e8b1ec5c274ac166bb4513608965215c246a24
Arunkumar Dhananjayan <arunkd13@gmail.com>
2025-11-15 15:09:25 +0530

    Rendering HTML using Tera

--- src/serve.rs
+++ src/serve.rs
@@ -1,6 +1,10 @@
 use std::path::Path;
 use anyhow::{Context, Result, bail};
-use axum::{Router, http::StatusCode, response::{IntoResponse, Response}, routing::get};
+use axum::{Router, http::StatusCode, response::{Html, IntoResponse, Response}, routing::get};
+use include_dir::{Dir, include_dir};
+use tera::Tera;
+
+static TEMPLATES_DIR: Dir = include_dir!("$CARGO_MANIFEST_DIR/templates");
 
 struct AppError(anyhow::Error);
 
@@ -41,6 +45,20 @@ pub async fn run(_db_path: &Path) -> Result<()> {
 }
 
 #[axum::debug_handler]
-async fn root() -> Result<String, AppError> {
-    Ok(String::from("Hello world!"))
+async fn root() -> Result<Html<String>, AppError> {
+    let mut tera = Tera::default();
+    // Iterate over the files in the embedded directory.
+    for file in TEMPLATES_DIR.files() {
+        let path = file.path().to_str().expect("Path is not valid UTF-8");
+        let content = file.contents_utf8().expect("Template file is not valid UTF-8");
+        
+        // Add the template to Tera. We use the file's path as the template name.
+        if let Err(e) = tera.add_raw_template(path, content) {
+            eprintln!("Error loading template {}: {}", path, e);
+        }
+    }
+    let context = tera::Context::new();
+    let body = tera.render("index.html", &context)?;
+
+    Ok(Html(body))
 }
